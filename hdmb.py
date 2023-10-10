31130bcae94086fb2d28b75e250987d3cdf66a79
lbnofficehpbook <18961312677@189.cn>
2023-10-10 11:48:36 +0800

    1

--- hdmb.py
+++ hdmb.py
@@ -4,37 +4,45 @@ import tkinter as tk
 from tkinter import filedialog
 
 filename =""
+def askfile():
+    # 从本地选择一个文件，并返回文件的目录
+    filename = tkinter.filedialog.askopenfilename()
+    if filename != '':
+         lb.config(text= filename)
+    else:
+         lb.config(text='您没有选择任何文件')
+root_window =tk.Tk()
+# 设置窗口title
+root_window.title('C语言中文网：c.biancheng.net')
+# 设置窗口大小:宽x高,注,此处不能为 "*",必须使用 "x"
+root_window.geometry('450x300')
 
-root = tk.Tk()
-root.withdraw()
+# 添加文本内,设置字体的前景色和背景色，和字体类型、大小
+text=tk.Label(root_window,text="政务短信基站模板生成",bg="yellow",fg="red",font=('Times', 20, 'bold italic'))
+# 将文本内容放置在主窗口内
+text.pack()
+#添加一个字符输入控件
+entry=tk.Entry(root_window,text="请输入字符")
+entry.pack()
+btn=tk.Button(root_window,text='选择文件',relief=RAISED,command=askfile)
+# 添加按钮，以及按钮的文本，并通过command 参数设置关闭窗口的功能
+button=tk.Button(root_window,text="关闭",command=root_window.quit)
+# 将按钮放置在主窗口内
+button.pack(side="bottom")
+#进入主循环，显示主窗口
+root_window.mainloop()
+#root.withdraw()
     # 选择文件夹
    # Folderpath = filedialog.askdirectory()
     # 选择文件
-Filepath = filedialog.askopenfilename()
+#Filepath = filedialog.askopenfilename()
     # 打印文件夹路径
     #print('Folderpath:', Folderpath)
     # 打印文件路径
-print('Filepath:', Filepath)
-filename = Filepath   
-print('Filename:', filename) 
-
-
 
 #基站模板管理
-# dirs = os.listdir(".")
-# filename = "未找到基站文件"
+filename = ""  
 
-# print("文件夹下所有文件列表:", dirs)
-# for i in dirs:
-#     #print("i:"+i+"   ++++"+i[-5:] , i[-4:] )
-#     if i[-5:] == ".xlsx" or i[-4:] == ".xls":
-#         filename = i
-#         break
-
-# if filename=="未找到基站文件":
-#     print("没找到基站文件") 
-#     exit()`
-print('Filename:', filename) 
 print("基站文件为：  "+(filename))
 
 wb = openpyxl.load_workbook(filename)

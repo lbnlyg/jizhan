d05262d4e10a19483365d93b4f12007b9c88ba2d
lbnofficehpbook <18961312677@189.cn>
2023-10-10 09:24:54 +0800

    1

--- backupcode.py
+++ backupcode.py
@@ -1,7 +1,8 @@
 import os
 dirs = os.listdir(".")
-filename = "未找到基站文件"
 
+#在本地目录下搜索所有xlsx,xls表， 找到的第一个文件
+filename = "未找到基站文件"
 print("文件夹下所有文件列表:", dirs)
 for i in dirs:
     #print("i:"+i+"   ++++"+i[-5:] , i[-4:] )
@@ -11,6 +12,6 @@ for i in dirs:
 
 if filename=="未找到基站文件":
     print("没找到基站文件") 
-    exit()`
+    exit()
 print('Filename:', filename) 
 print("基站文件为：  "+(filename))

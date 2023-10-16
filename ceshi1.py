cae71a777eaa39859a5d2210a7ca87a2077ead8a
lbnofficehpbook <18961312677@189.cn>
2023-10-16 17:50:12 +0800

    yunxingzhong

--- ceshi1.py
+++ ceshi1.py
@@ -1,21 +1,14 @@
-from tkinter import *
-import tkinter.filedialog  # 注意次数要将文件对话框导入
-# 定义一个处理文件的相关函数
-def askfile():
-    # 从本地选择一个文件，并返回文件的目录
-    filename = tkinter.filedialog.askopenfilename()
-    if filename != '':
-         lb.config(text= filename)
-    else:
-         lb.config(text='您没有选择任何文件')
-root = Tk()
-root.config(bg='#87CEEB')
-root.title("C语言中文网")
-root.geometry('400x200+300+300')
+count = 30888 #麦叔粉丝数
 
-btn=Button(root,text='选择文件',relief=RAISED,command=askfile)
-btn.grid(row=0,column=0)
-lb = Label(root,text='',bg='#87CEEB')
-lb.grid(row=0,column=1,padx=5)
-# 显示窗口
-root.mainloop()
+# 关注
+def guanzhu():
+    count = count + 1
+    print('麦叔的粉丝数是{}'.format(count))
+
+# 取关
+def quguan():
+    count = count - 1
+    print('麦叔的粉丝数是{}'.format(count))
+
+guanzhu()
+quguan()
--- hdmb.py
+++ hdmb.py
@@ -9,10 +9,14 @@ def askfile():
     # 从本地选择一个文件，并返回文件的目录
     filename = filedialog.askopenfilename(title="请选择基站表格文件",initialdir='.\\')
     if filename != '':
-            lb.config(text= filename)
-            print("基站文件为：  "+filename)
+                if filename[-5:] == ".xlsx" or filename[-4:] == ".xls":
+                    lb.config(text=  "您选择的基站文件为： "+ filename)
+                else:
+                    lb.config(text="您选择的文件并非EXCEL表格基站文件")
+                    return
     else:
             lb.config(text='您没有选择任何文件')
+            return
     
     wb = openpyxl.load_workbook(filename)
     # 显示所有表名
@@ -51,29 +55,39 @@ root_window =tk.Tk()
 root_window.iconbitmap("tubiao.ico")
 # 设置窗口title
 root_window.title('政务短信处理助手')
-
-
 # 设置窗口大小:宽x高,注,此处不能为 "*",必须使用 "x"
-root_window.geometry('450x300')
+root_window.geometry('1000x600')
+
+
+frame_left_top = tk.LabelFrame(root_window,bg="blue",highlightthickness=10,bd=20)
+frame_left_buttom = tk.LabelFrame(root_window,bg="green",highlightthickness=10,bd=20)
+frame_right_top = tk.LabelFrame(root_window)
+frame_right_buttom = tk.LabelFrame(root_window)
+
+frame_left_top.pack(anchor="nw")
+frame_left_buttom.pack(anchor="ne")
+
 
 # 添加文本内,设置字体的前景色和背景色，和字体类型、大小
-text=tk.Label(root_window,text="政务短信基站模板生成",bg="yellow",fg="red",font=('Times', 20, 'bold italic'))
+text=tk.Label(frame_left_top,text="政务短信基站模板生成",bg="yellow",fg="red",font=('Times', 20, 'bold italic'))
 # 将文本内容放置在主窗口内
 text.pack()
 #添加一个字符输入控件
 
-btn=tk.Button(root_window,text='选择文件',command=askfile)
+btn=tk.Button(frame_left_top,text='选择文件',command=askfile)
 btn.pack()
+
+lb = tk.Label(frame_left_top,text='',bg='#87CEEB')
+lb.pack()
+
+lb2 = tk.Label(frame_left_top,text='',bg='#87CEEB')
+lb2.pack()
 # 添加按钮，以及按钮的文本，并通过command 参数设置关闭窗口的功能
 button=tk.Button(root_window,text="关闭",command=root_window.quit)
 # 将按钮放置在主窗口内
 button.pack(side="bottom")
 
-lb = tk.Label(root_window,text='',bg='#87CEEB')
-lb.pack()
 
-lb2 = tk.Label(root_window,text='',bg='#87CEEB')
-lb2.pack()
 #基站模板管理
 #c=tk.Label(root_window,text="基站文件为：  "+filename,bg="yellow",fg="red",font=('Times', 20, 'bold italic'))
 #c.pack()
